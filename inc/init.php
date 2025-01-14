f9f01a44ccb44533c2892bd769954aa351a19b5e
“WebstartMaterial” <web.start.contact@gmail.com>
2025-01-14 11:39:16 +0100

    maj db

--- inc/init.php
+++ inc/init.php
@@ -1,7 +1,7 @@
 <?php
 
 // Connexion à la BDD
-$pdo = new PDO('mysql:host=localhost;dbname=shop','root','root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$pdo = new PDO('mysql:host=ovftiguihor.mysql.db;dbname=ovftiguihor','ovftiguihor','Ihor2024', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 require_once("fonction.php");
 
